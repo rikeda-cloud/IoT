820952d36b78bb48c4d9ad52424c486b3c1856f5
rikeda-cloud <longsichitian648@gmail.com>
2025-03-28 23:03:02 +0900

    [update] IoTDataモデルを作成

--- api/srcs/models.py
+++ api/srcs/models.py
@@ -1,11 +1,16 @@
 from app import db
+from sqlalchemy.sql import func
 
 
-# モデルの定義（テーブルの作成）
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+# デバイスのデータテーブルの定義
+class IoTData(db.Model):
+    __tablename__ = "iot_data"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    device_id = db.Column(db.Integer, nullable=False)
+    temperature = db.Column(db.Float, nullable=False)
+    humidity = db.Column(db.Float, nullable=False)
+    timestamp = db.Column(db.DateTime, default=func.now(), nullable=False)
 
     def __repr__(self):
-        return f"<User {self.username}>"
+        return f"<DeviceData device_id={self.device_id} temperature={self.temperature} humidity={self.humidity} timestamp={self.timestamp}>"
