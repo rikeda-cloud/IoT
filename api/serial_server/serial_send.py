3d04c5104fc8fb00f726a8cacb70336de35fb7bd
rikeda-cloud <longsichitian648@gmail.com>
2025-03-29 12:37:55 +0900

    [update] シリアル通信で受け取ったデータをWebサーバにPostする処理を追加

--- api/serial_server/serial_send.py
+++ api/serial_server/serial_send.py
@@ -1,9 +1,16 @@
 import serial
+import requests
 
 # シリアルポートを開く（例：/dev/ttyUSB0）
 ser = serial.Serial("/dev/ttyUSB0", 115200)
 
+url = "http://127.0.0.1:8080/api/iot-data"
+
 while True:
     # シリアルポートからデータを受信
     data = ser.readline().decode("utf-8").strip()
-    print(f"Received: {data}")
+    requests.post(url, data=data)
+    response = requests.post(
+        url, data=data, headers={"Content-Type": "application/json"}
+    )
+    print(data)
